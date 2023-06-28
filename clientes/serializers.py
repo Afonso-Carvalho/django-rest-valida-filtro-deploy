444590f99a63997a8e811ba0d201129ebcdf45df
Afonso-Carvalho <afonsox57@gmail.com>
2023-06-28 16:58:24 -0300

    Importando validações de cpf e gerando novos clientes

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -15,8 +15,8 @@ class ClienteSerializer(serializers.ModelSerializer):
             errors['nome'] = "Não inclua números neste campo"
         if not rg_valido(data['rg']):
             errors['rg'] = "O RG deve ter 9 dígitos"
-        if celular_valido(data['celular']):
-            errors['celular'] = "O número de celular deve ter no minimo 11 digitos"
+        if not celular_valido(data['celular']):
+            errors['celular'] = "O número de celular deve seguir este modelo: 11 91234-1234 (respeitando os espaços e traço)"
 
         if errors:
             raise serializers.ValidationError(errors)
--- clientes/validators.py
+++ clientes/validators.py
@@ -1,6 +1,9 @@
-#Lembrando tem q ter escrito validate certo se n ele não entede como medoto de validação 
-def cpf_valido(numero_do_cpf): # valida se o cpf tem 11 digitos
-    return len(numero_do_cpf) == 11
+import re
+from validate_docbr import CPF
+
+def cpf_valido(numero_do_cpf): # valida o cpf
+    cpf = CPF()
+    return cpf.validate(numero_do_cpf)
 
 def nome_valido(nome):
     return nome.isalpha()
@@ -9,4 +12,7 @@ def rg_valido(numero_do_rg):
     return len(numero_do_rg) == 9
 
 def celular_valido(numero_do_celular):
-    return len(numero_do_celular) < 11
+    """Verifica se o celualr e valido (11 912341234)"""
+    modelo = '[0-9]{2} [0-9]{5}-[0-9]{4}'
+    resposta  = re.findall(modelo,numero_do_celular)
+    return resposta
